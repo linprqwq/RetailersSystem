64e9896b3d5a42aa877fc8aa7c54f1f390575722
linprqwq797 <317348898@qq.com>
2022-03-27 17:26:09 +0800

    登录

---
+++ src/component/Login.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <form id="myform">
+      <input type="text"  v-model="zhh" placeholder="请输入账号">
+      <input type="password" v-model="pwd"  placeholder="请输入密码">
+      <input type="submit" id="btn01" value="登录" @click="logindl">
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+
+  name: "Login",
+  data(){
+    return{
+      zhh:"",
+      pwd:""
+    }
+  },
+  methods:{
+    logindl(){
+      var params= new URLSearchParams();
+      params.append("loginname",this.zhh)
+      params.append("password",this.pwd)
+      this.$axios.post("login.action",params).then(
+        val=>{
+          if(val==0){
+            alert("账号密码有误")
+          }
+        }
+      ).catch()
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,16 @@
 import Vue from 'vue'
-import App from './App.vue'
+import Login from "./component/Login";
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(ElementUI)
+//axios
+import  Axios from 'axios'
+//设置axios默认访问路径
+Axios.defaults.baseURL = "http://127.0.0.1:9090/RetailersBackSystem"
+// 将baseURL绑定到全局
+Vue.prototype.$axios = Axios
 
 new Vue({
   el: '#app',
-  render: h => h(App)
+  render: h => h(Login)
 })

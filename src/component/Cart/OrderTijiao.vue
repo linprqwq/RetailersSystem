20ec5b78193f7465f38241d782c9f903ff1e6dbc
xxs1.2.3 <1530809506@qq.com>
2022-04-13 15:29:16 +0800

    提交订单

--- src/component/Cart/OrderTijiao.vue
+++ src/component/Cart/OrderTijiao.vue
@@ -26,8 +26,6 @@
     <h3 class="common_title">支付方式</h3>
     <div class="common_list_con clearfix">
       <div class="pay_style_con clearfix">
-        <input type="radio" name="pay_style" checked>
-        <label class="weixin">微信支付</label>
         <input type="radio" name="pay_style">
         <label ><span>余额:{{user.umoney}}</span></label>
       </div>

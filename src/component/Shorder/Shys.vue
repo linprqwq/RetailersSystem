515767617b893d6aba381d875de6cd63e706ed27
hjy <3293541598@qq.com>
2022-04-19 15:51:34 +0800

    营收100%

--- src/component/Shorder/Shys.vue
+++ src/component/Shorder/Shys.vue
@@ -8,66 +8,88 @@
 
     export default {
         name: "Shys",
+
       data(){
           return{
+            arr1:[],
+            arr2:[],
             useridd:sessionStorage.getItem('id'),
+
           }
       },
       methods:{
+      shys(){
+        console.log("2")
+        var params=new URLSearchParams();
+        params.append("uid",1)
+        this.$axios.post("qeruyshys.action",params).then(res=>{
+
+          this.$nextTick(item=>{
+            this.arr1.push(res.data[0].mm);
+            this.arr2.push(res.data[0].smoney);
+          })
+          this.getdata();
+
+
+        }).catch()
+      },
+
         getdata() {
+          console.log("1")
           // 基于准备好的dom，初始化echarts实例
           var myChart = echarts.init(document.getElementById('main'));
-          var option;
-          option = {
+          var _this =this;
+          var option ={
             tooltip: {
               trigger: 'axis',
-              axisPointer: {
+                axisPointer: {
                 type: 'shadow'
               }
             },
             grid: {
               left: '3%',
-              right: '4%',
-              bottom: '3%',
-              containLabel: true
+                right: '4%',
+                bottom: '3%',
+                containLabel: true
             },
             xAxis: [
               {
                 type: 'category',
-                data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+                data: _this.arr1,
                 axisTick: {
                   alignWithLabel: true
                 }
               }
             ],
-            yAxis: [
+              yAxis: [
               {
                 type: 'value'
               }
             ],
-            series: [
+              series: [
               {
                 name: 'Direct',
                 type: 'bar',
                 barWidth: '60%',
-                data: [10, 52, 200, 334, 390, 330, 220]
+                data: _this.arr2
               }
             ]
-          };
-          myChart.setOption(option);
-          var params=new URLSearchParams()
-          params.append("uid",this.useridd)
-          this.$axios.post("qeruyshys.action",params).then(res=>{
+          }
+          this.$nextTick(item=>{
+            myChart.setOption(option);
+          })
 
-
-            }
-          )
         }
+      },
+      created() {
+
       },
       //注意：我们要在mounted生命周期函数中实例化echarts对象。因为我们要确保dom元素已经挂载到页面中
       mounted() {
-        this.getdata();
+        this.shys()//this.getdata();
       }
+
+
     }
 </script>
 

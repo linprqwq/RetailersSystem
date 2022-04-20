cfa9ed6f239dab2284c47b37cd1987fbcf436ed6
xxs1.2.3 <1530809506@qq.com>
2022-04-20 19:41:39 +0800

    仓库详情

--- src/component/Ck/AddWareHouse.vue
+++ src/component/Ck/AddWareHouse.vue
@@ -1,36 +1,36 @@
 <template>
-<div>
-  <el-form :model="AddWhouse" :rules="rules" ref="awhref" label-width="100px" class="demo-ruleForm">
-    <el-form-item label="仓库名" prop="warName">
-      <el-input v-model="AddWhouse.warName"></el-input>
-    </el-form-item>
-    <el-form-item label="最大存储数" prop="warMaxStock">
-      <el-input v-model.number="AddWhouse.warMaxStock" ></el-input>
-    </el-form-item>
-    <el-form-item>
-      <el-select v-model="fl" multiple placeholder="请选择">
-        <el-option
-          v-for="item in fllist"
-          :key="item.id"
-          :label="item.name"
-          :value="item.id">
-        </el-option>
-      </el-select>
-    </el-form-item>
-  </el-form>
-</div>
+  <div>
+    <el-form :model="AddWhouse" :rules="rules" ref="awhref" label-width="100px" class="demo-ruleForm">
+      <el-form-item label="仓库名" prop="warName">
+        <el-input v-model="AddWhouse.warName"></el-input>
+      </el-form-item>
+      <el-form-item label="最大存储数" prop="warMaxStock">
+        <el-input v-model.number="AddWhouse.warMaxStock"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-select v-model="fl" multiple placeholder="请选择">
+          <el-option
+            v-for="item in fllist"
+            :key="item.id"
+            :label="item.name"
+            :value="item.id">
+          </el-option>
+        </el-select>
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
 
 <script>
 export default {
   name: "AddWareHouse",
-  data(){
+  data() {
     var checkAge = (rule, value, callback) => {
       setTimeout(() => {
         if (!Number.isInteger((Number(value)))) {
           callback(new Error('只能是数字'));
         } else {
-          if (value<1) {
+          if (value < 1) {
             callback(new Error('不能为空'));
           } else {
             callback();
@@ -38,39 +38,37 @@ export default {
         }
       }, 1000);
     };
-    return{
+    return {
 
-      AddWhouse: {warName:"",warMaxStock:""},
-      fl:[],
-      fllist:[],
+      AddWhouse: {warName: "", warMaxStock: ""},
+      fl: [],
+      fllist: [],
 
-      rules:{
-        warName:[{ required: true, message: '仓库名不能为空' ,trigger: 'click'}],
-        warMaxStock:[{validator:checkAge,trigger: 'click' }],
+      rules: {
+        warName: [{required: true, message: '仓库名不能为空', trigger: 'click'}],
+        warMaxStock: [{validator: checkAge, trigger: 'click'}],
       },
 
     }
   },
-  props:{
-
-  },
-  methods:{
-    addcka(formName){
+  props: {},
+  methods: {
+    addcka(formName) {
       this.$refs[formName].validate((valid) => {
         if (valid) {
-          if (this.fl.length==0){
+          if (this.fl.length == 0) {
             this.$message.error("请选择商品分类");
-            return ;
+            return;
           }
           var params = new URLSearchParams();
-          params.append("warName",this.AddWhouse.warName);
-          this.$axios.post("cxwhouse.action",params).then(res=>{
-              if (res.data.code==1){
-                this.$message.error(res.data.msg);
-                return;
-              }else{
-                this.tjck();
-              }
+          params.append("warName", this.AddWhouse.warName);
+          this.$axios.post("cxwhouse.action", params).then(res => {
+            if (res.data.code == 1) {
+              this.$message.error(res.data.msg);
+              return;
+            } else {
+              this.tjck();
+            }
           }).catch()
         } else {
           console.log('error submit!!');
@@ -80,31 +78,31 @@ export default {
       });
     },
     //商品分类查询
-    queryspfenlei(){
-      this.$axios.get("shopinfo.action/queryallshoptype.action").then(res=>{
+    queryspfenlei() {
+      this.$axios.get("shopinfo.action/queryallshoptype.action").then(res => {
         this.fllist = res.data;
         console.log(this.fllist);
       }).catch()
     },
-    tjck(){
+    tjck() {
 
       var params = new URLSearchParams();
-      params.append("warName",this.AddWhouse.warName);
-      params.append("warMaxStock",this.AddWhouse.warMaxStock);
-      params.append("arr",this.fl)
+      params.append("warName", this.AddWhouse.warName);
+      params.append("warMaxStock", this.AddWhouse.warMaxStock);
+      params.append("arr", this.fl)
 
-      this.$axios.post("tjck.action",params).then(res=>{
-          if (res.data.code==0){
-            this.$message.error(res.data.msg);
-          }else{
-            this.$message.success(res.data.msg);
-            this.$emit('success',true);
-          }
+      this.$axios.post("tjck.action", params).then(res => {
+        if (res.data.code == 0) {
+          this.$message.error(res.data.msg);
+        } else {
+          this.$message.success(res.data.msg);
+          this.$emit('success', 1);
+        }
       }).catch();
     }
   },
   created() {
-  this.queryspfenlei();
+    this.queryspfenlei();
   }
 }
 </script>
--- src/component/Ck/CkView.vue
+++ src/component/Ck/CkView.vue
@@ -2,10 +2,12 @@
   <div>
 
     <el-input v-model="ckmName" placeholder="请输入您要查询的仓库名" style="width: 200px;"></el-input>
-    <el-button type="primary" @click="queryallck()">查询</el-button><br><br>
+    <el-button type="primary" @click="queryallck()">查询</el-button>
+    <br><br>
     <el-button type="primary" @click="addwarehouse()" style="position: relative;left: -600px">添加仓库</el-button>
 
-    <el-button icon="el-icon-refresh-left" circle @click="queryallck" style="position: relative;left: 600px"></el-button>
+    <el-button icon="el-icon-refresh-left" circle @click="queryallck"
+               style="position: relative;left: 600px"></el-button>
     <el-table
       :data="ckall"
       style="width: 100%">
@@ -20,24 +22,26 @@
 
       <el-table-column label="仓库分类" width="180">
 
-          <template slot-scope="scope">
-            <el-popover trigger="hover" placement="top">
-              <p>{{ scope.row.shoptypename }}</p>
-              <div slot="reference" class="name-wrapper">
-                <el-tag size="medium">查看分类详情</el-tag>
-              </div>
-            </el-popover>
-          </template>
+        <template slot-scope="scope">
+          <el-popover trigger="hover" placement="top">
+            <p>{{ scope.row.shoptypename }}</p>
+            <div slot="reference" class="name-wrapper">
+              <el-tag size="medium">查看分类详情</el-tag>
+            </div>
+          </el-popover>
+        </template>
       </el-table-column>
       <el-table-column>
         <template slot-scope="scope">
           <el-button
             size="mini"
-            @click="updatewarehoures(scope.$index, scope.row)">修改仓库</el-button>
+            @click="updatewarehoures(scope.$index, scope.row)">修改仓库
+          </el-button>
           <el-button
             size="mini"
             type="danger"
-            @click="querywarexqbyid(scope.$index, scope.row)">查看仓库详情</el-button>
+            @click="querywarexqbyid(scope.$index, scope.row)">查看仓库详情
+          </el-button>
         </template>
       </el-table-column>
 
@@ -59,9 +63,9 @@
       width="30%"
       :before-close="handleClose">
       <!-- 动态组件   指定添加vue页面在模态框显示-->
-      <component ref="awhref" is="AddWareHouse"v-on:success="qx(true)" ></component>
+      <component ref="awhref" is="AddWareHouse" v-on:success="qx(1)"></component>
       <el-button type="primary" @click="AddWHouse('awhref')">确 定</el-button>
-      <el-button @click="qx(true)">取 消</el-button>
+      <el-button @click="qx(1)">取 消</el-button>
     </el-dialog>
     <!--  修改页面-->
     <el-dialog
@@ -70,9 +74,21 @@
       width="30%"
       :before-close="handleClose">
       <!-- 动态组件   指定添加vue页面在模态框显示-->
-      <component ref="updateckref" is="UpdateCk"v-on:success="qx(false)" ></component>
+      <component ref="updateckref" is="UpdateCk" v-on:success="qx(2)"></component>
       <el-button type="primary" @click="UpdateWareHouse('updateckref')">确 定</el-button>
-      <el-button @click="qx(false)">取 消</el-button>
+      <el-button @click="qx(2)">取 消</el-button>
+
+    </el-dialog>
+    <!--  仓库详情页面-->
+    <el-dialog
+      title="仓库详情"
+      :visible.sync="CkDetails"
+      width="90%"
+      :before-close="handleClose">
+      <!-- 动态组件   指定添加vue页面在模态框显示-->
+      <component ref="ckdetailsref" is="CkDetails" v-on:success="qx(3)"></component>
+      <el-button type="primary" @click="UpdateWareHouse('ckdetailsref')">确 定</el-button>
+      <el-button @click="qx(3)">取 消</el-button>
 
     </el-dialog>
   </div>
@@ -81,34 +97,38 @@
 <script>
 import AddWareHouse from "./AddWareHouse";
 import UpdateCk from "./UpdateCk";
+import CkDetails from "./CkDetails";
+
 export default {
   name: "CkView",
-  data(){
-    return{
-      ckall:[],
-      pageno:1,   //页码
-      pagesize:5,   //页size
-      total:1,   //查询到的总记录数量
+  data() {
+    return {
+      ckall: [],
+      pageno: 1,   //页码
+      pagesize: 5,   //页size
+      total: 1,   //查询到的总记录数量
       search: '',
-      ckmName:'',
-      AddCk:false,
-      UpdateCk:false,
+      ckmName: '',
+      AddCk: false,
+      UpdateCk: false,
+      CkDetails:false,
     }
   },
-  components:{
+  components: {
     AddWareHouse,
     UpdateCk,
+    CkDetails,
   },
-  methods:{
+  methods: {
     //查询仓库
-    queryallck(){
+    queryallck() {
       var params = new URLSearchParams();
-      params.append("pageno",this.pageno);
-      params.append("pagesize",this.pagesize);
+      params.append("pageno", this.pageno);
+      params.append("pagesize", this.pagesize);
       //仓库名
-      params.append("warName",this.ckmName);
+      params.append("warName", this.ckmName);
 
-      this.$axios.post("queryallck.action",params).then(res=>{
+      this.$axios.post("queryallck.action", params).then(res => {
         this.total = res.data.total; //总记录数量
         this.ckall = res.data.records;
 
@@ -116,65 +136,82 @@ export default {
     },
     //分页控件
     handleSizeChange(val) { //分页控件  页面size改变 触发  val参数就是选择的条数
-      this.pagesize= val;
+      this.pagesize = val;
       this.queryallck()
     },
     //页码发生改变
     handleCurrentChange(val) { //页码改变 触发  val  当前准备跳转的页码
-      this.pageno=val;
+      this.pageno = val;
       this.queryallck()
 
     },
     //修改仓库
-    updatewarehoures(index,wh){
+    updatewarehoures(index, wh) {
       //模态框展示
       this.UpdateCk = true;
-      this.$nextTick(item=>{
+      this.$nextTick(item => {
         this.$refs.updateckref.warehousemtkzs(wh);
       })
 
     },
     //查看商品详情
-    querywarexqbyid(){
+    querywarexqbyid(index,row) {
       //模态框展示
+      this.CkDetails = true;
+      this.$nextTick(item => {
+        this.$refs.ckdetailsref.querywaredetails(row);
+      })
     },
     //打开添加模态框
-    addwarehouse(){
-    this.AddCk = true;
+    addwarehouse() {
+      this.AddCk = true;
     },
     //窗口关闭确认
     handleClose(done) {
       this.$confirm('确认关闭？')
         .then(_ => {
-          this.AddCk = false
+
+          this.CkDetails= false;
+          this.$refs.ckdetailsref.ckdetails = [];
+
+          this.AddCk = false;
+          this.$refs.awhref.AddWhouse = {};
+          this.$refs.awhref.fl = [];
           this.UpdateCk = false;
-          this.$refs.updateckref.value=[];
-          this.$refs.updateckref.mtkwarehouse= {};
-          this.$refs.awhref.AddWhouse={};
-          this.$refs.awhref.fl=[];
+
+          this.$refs.updateckref.value = [];
+          this.$refs.updateckref.mtkwarehouse = {};
+
+
+
+
         })
-        .catch(_ => {});
+        .catch(_ => {
+        });
     },
     //添加仓库
-    AddWHouse(formName){
+    AddWHouse(formName) {
       this.$refs.awhref.addcka(formName);
     },
     //修改仓库
-    UpdateWareHouse(formName){
+    UpdateWareHouse(formName) {
       this.$refs.updateckref.UpdateWareHouse(formName);
     },
     //取消模态框
-    qx(boole){
-      if (boole){
+    qx(boole) {
+      if (boole==1) {
         this.AddCk = false
-        this.$refs.awhref.AddWhouse={};
-        this.$refs.awhref.fl=[];
+        this.$refs.awhref.AddWhouse = {};
+        this.$refs.awhref.fl = [];
         this.queryallck();
-      }else{
+      } else if(boole==2) {
         this.UpdateCk = false;
-        this.$refs.updateckref.value=[];
-        this.$refs.updateckref.mtkwarehouse= {};
+        this.$refs.updateckref.value = [];
+        this.$refs.updateckref.mtkwarehouse = {};
         this.queryallck();
+      }else if (boole==3){
+        this.CkDetails =false;
+        this.$refs.ckdetailsref.ckdetails = [];
       }
     }
   },
@@ -190,6 +227,7 @@ export default {
   cursor: pointer;
   color: #409EFF;
 }
+
 .el-icon-arrow-down {
   font-size: 12px;
 }
